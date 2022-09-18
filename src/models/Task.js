2cbf07143a11e7c7deaebf0ffba61d90e44d1d74
zaidAljehni <zaidddavid7@gmail.com>
2022-09-18 12:38:39 +0300

    Create Task model

---
+++ src/models/Task.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Task name must be provided."],
+        trim: true,
+        maxlength: [20, "Task name' length must be 20 at maximum."]
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model("Task", taskSchema);
