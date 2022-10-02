ba322a34f8eb79235bd61d324d8498918b22daec
zaidAljehni <zaidddavid7@gmail.com>
2022-10-02 21:53:11 +0300

    add relation with User model in Task model

--- src/models/Task.js
+++ src/models/Task.js
@@ -10,6 +10,10 @@ const taskSchema = new mongoose.Schema({
     completed: {
         type: Boolean,
         default: false
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
     }
 });
 
